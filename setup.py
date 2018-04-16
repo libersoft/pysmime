dff000600c016d70c2315edf9b74cca85acad094
Lorenzo Gaggini <lg@lgaggini.net>
2018-04-16 16:12:30 +0200

    updated setup.py for new pypi release

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from distutils.core import setup
+from setuptools import setup
 
 with open('requirements.txt') as f:
     install_requirements = f.readlines()
@@ -10,10 +10,10 @@ with open('README.md') as file:
     long_description = file.read()
 
 setup(
-    name='pysmime',
-    version='0.1.0',
+    name='pysmime3',
+    version='0.2.0',
     author='Lorenzo Gaggini',
-    author_email='lg@libersoft.it',
+    author_email='lg@lgaggini.net',
     packages=['pysmime'],
     url='http://libersoft.github.com/pysmime',
     license='LICENSE',
@@ -22,7 +22,7 @@ setup(
     long_description=long_description,
     classifiers=[
         'Programming Language :: Python',
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
         'Environment :: Other Environment',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
